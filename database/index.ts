ec58e5315bde4eb57b071a61db6970b42a566830
Sohaib Shah <turbosohaib1234@gmail.com>
2025-10-31 08:56:27 +0500

    Implemented database models

--- database/index.ts
+++ database/index.ts
@@ -1,2 +1,7 @@
-export * from "./booking.model";
-export * from "./event.model";
+// Database models exports
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
+
+// TypeScript interfaces exports
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
